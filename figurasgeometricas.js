3372102d069e0519265429c9a103c2521d8d778e
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-07-17 10:22:24 -0500

    add/ aras y perimetros de Cuadrado, triangulo y circulo

--- figurasgeometricas.js
+++ figurasgeometricas.js
@@ -17,9 +17,9 @@
 
 //Código del cuadrado
 console.group("Cuadrado");
+
 const ladoCuadrado = 5;
 console.log("Los lados del cuadrado valen:" + ladoCuadrado + "cm");
-
 
 const perimetroCuadrado = ladoCuadrado*4;
 console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cm");
@@ -28,9 +28,9 @@ const areaCuadrado = ladoCuadrado * ladoCuadrado;
 console.log("El area del cuadrado es: " + areaCuadrado + "cm^2");
 console.groupEnd();
 
+
 //Código del triangulo
 console.group("Triangulo");
-
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const baseTriangulo = 4;
@@ -42,11 +42,30 @@ const perimetroTriangulo = ladoTriangulo1+ladoTriangulo2+baseTriangulo;
 console.log("El perimetro del triangulo es:" + perimetroTriangulo + "cm");
 
 const areaTriangulo = (baseTriangulo*alturaTriangulo)/2;
-console.log("El Area del triauglo mide: "+alturaTriangulo)
-
+console.log("El Area del triauglo mide: "+alturaTriangulo);
 
 console.groupEnd();
 
 
+//Código del Circulo
 
+console.group("Circulo");
+//Radio
+const radioCirculo = 4;
+
+//Diametro
+const diametroCirculo = radioCirculo*2;
+
+//Perimetro
+const PI = Math.PI;
+console.log(PI);
+
+const perimetroCirculo = diametroCirculo*PI;
+console.log("El perimetro del Circulo es:", perimetroCirculo);
+
+//Area
+const areaCirculo = (radioCirculo*radioCirculo)*PI;
+console.log("El area del curculo es:", areaCirculo);
+
+console.groupEnd();
 
